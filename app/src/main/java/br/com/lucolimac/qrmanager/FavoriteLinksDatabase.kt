c45dfdabaf5153d79f49d428c81b2c80e234852a
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2023-12-04 20:55:48 -0300

    feature(layouts): Added database

---
+++ app/src/main/java/br/com/lucolimac/qrmanager/FavoriteLinksDatabase.kt
@@ -0,0 +1,27 @@
+package br.com.lucolimac.qrmanager
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import br.com.lucolimac.qrmanager.data.FavoriteLink
+import br.com.lucolimac.qrmanager.data.FavoriteLinkDAO
+
+@Database(entities = [FavoriteLink::class], version = 1)
+abstract class FavoriteLinksDatabase : RoomDatabase() {
+    abstract fun contactDAO(): FavoriteLinkDAO
+
+    companion object {
+        @Volatile
+        private var INSTANCE: FavoriteLinksDatabase? = null
+        fun getDatabase(context: Context): FavoriteLinksDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext, FavoriteLinksDatabase::class.java, "favoritelink.db"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
